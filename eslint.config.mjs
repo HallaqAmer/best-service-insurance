3027d7ba1c8e46cbe267b0033aa8d700fccf06f0
Amer Hallaq <amerhalaq91@gmail.com>
2025-02-22 13:52:54 +0200

    removed files for initial deployment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "react/jsx-key": "warn",
+      "no-console": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
